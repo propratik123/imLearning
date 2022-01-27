2f557dab3055d6e03b9cee359e3093fd3d4a84a6
pratik <pratikpetkar231@gmail.com>
2022-01-27 20:23:21 +0530

    ass3 complete 23jan

---
+++ .vscode/ass3.js
@@ -0,0 +1,15 @@
+for(i=1; i<=1; i++){
+    console.log(i)
+}
+for(i=2; i<=2; i++){
+    console.log(i+"2")
+}
+for(i=3; i<=3; i++){
+    console.log(i+"33")
+}
+for(i=4; i<=4; i++){
+    console.log(i+"444")
+}
+for(i=5; i<=5; i++){
+    console.log(i+"5555")
+}
